1cdbc65339033bdf3f018bd174249e6f8bd6fe8f
Rupsssss <121189968+Rupsssss@users.noreply.github.com>
2024-09-12 08:58:33 -0400

    m
    m

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+    public bool startPlaying;
+    public BeatScoller theBS;
+    public static GameManager instance;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+
+    public Text scoreText;
+    public Text multiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        scoreText.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying) {
+            if (Input.anyKeyDown) {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+    }
+    public void NoteHit() 
+    {
+        Debug.Log("Hit On Time");
+
+        currentScore += scorePerNote;
+        scoreText.text = "Score: " + currentScore;
+    }
+    public void NoteMissed()
+    {
+        Debug.Log("Missed Note");
+    }
+}
